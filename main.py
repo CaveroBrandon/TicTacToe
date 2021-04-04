f1dd779bb335d5bef837a042673badcbfe8002c9
Brandon <77588516+CaveroBrandon@users.noreply.github.com>
2021-04-04 16:34:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Tic tac toe game, not ussing IA, just random numbers
-from random import randint
 from tkinter import *
-import sys
 
 
 def fun_console_version():
@@ -20,14 +18,16 @@ def fun_exit():
 
 root = Tk()
 root.title('TIC TAC TOE')
-#root. geometry('350x150+250+200')
+
 
 label1 = Label(root, text = 'Choose one option')
+
 
 #Buttons definitions
 boton1 = Button(root, text = 'Console Version', command = fun_console_version, height = 3, width = 20)
 boton2 = Button(root, text = 'Gui Version', command = fun_gui_version, height = 3, width = 20)
 boton3 = Button(root, text = 'EXIT', command = fun_exit, height = 1, width = 10)
+
 
 #Buttons positioning
 boton1.grid(row = 1, column = 0)
